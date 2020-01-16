fa83baa85dccda73adacd42b1537dce2caa22616
shuaipengge <shuaipengge@gmail.com>
2020-01-16 23:12:14 +0800

    fix css

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -48,18 +48,6 @@ export const NavItem = styled.div`
 export const SearchWrapper = styled.div`
   position: relative;
   float: left;
-  .slide-enter {
-    transition: all 0.2s ease-out;
-  }
-  .slide-enter-active {
-    width: 240px;
-  }
-  .slide-exit {
-    transition: all 0.2s ease-out;
-  }
-  .slide-exit-active {
-    width: 160px;
-  }
   .iconfont {
     position: absolute;
     right: 5px;
@@ -93,6 +81,18 @@ export const NavSearch = styled.input.attrs({ placeholder: "搜索" })`
   }
   &.focused {
     width: 240px;
+  }
+  &.slide-enter {
+    transition: all 0.2s ease-out;
+  }
+  &.slide-enter-active {
+    width: 240px;
+  }
+  &.slide-exit {
+    transition: all 0.2s ease-out;
+  }
+  &.slide-exit-active {
+    width: 160px;
   }
 `;
 
