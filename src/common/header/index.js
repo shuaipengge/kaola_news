496bfa9009359331479c1fcdf55bc439bdbeb97b
shuaipengge <shuaipengge@gmail.com>
2020-01-23 00:10:32 +0800

    登录鉴权

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -119,10 +119,12 @@ class Header extends Component {
           </SearchWrapper>
         </Nav>
         <Addition>
-          <Button className="writting">
-            <span className="iconfont">&#xe616;</span>
-            写文章
-          </Button>
+          <Link to="/write">
+            <Button className="writting">
+              <span className="iconfont">&#xe616;</span>
+              写文章
+            </Button>
+          </Link>
           <Button className="reg">注册</Button>
         </Addition>
       </HeaderWrapper>
